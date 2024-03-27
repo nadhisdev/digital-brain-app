ad6f56393b514de83700e6441b5aecfcc8589145
nadhisdev <developer@nadhis.com>
2024-03-27 09:24:23 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import { Toaster } from "sonner";
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+import { MantineProvider } from '@mantine/core';
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Nadhis Digital Brain',
+  description: 'The connected workspace where better, faster work happens.',
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon.png",
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/icon-192x192.png",
+        href: "/icon-192x192.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/icon-192x192-dark.png",
+        href: "/icon-192x192-dark.png",
+      }
+    ]
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className={inter.className}>
+      <MantineProvider>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="Nadhis Digital Brain-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </MantineProvider>
+      </body>
+    </html>
+  )
+}
---
+++ components/editor.tsx
@@ -0,0 +1,236 @@
+"use client";
+import { useTheme } from "next-themes";
+import {
+  defaultBlockSpecs,
+  defaultInlineContentSpecs,
+  BlockNoteSchema,
+  filterSuggestionItems,
+  insertOrUpdateBlock,
+} from "@blocknote/core";
+import {
+  BlockNoteView,
+  useCreateBlockNote,
+  getDefaultReactSlashMenuItems,
+  SuggestionMenuController,
+  SideMenuController,
+  SideMenu,
+  DragHandleMenu,
+  RemoveBlockItem,
+  BlockColorsItem,
+  DefaultReactSuggestionItem,
+} from "@blocknote/react";
+import "@blocknote/react/style.css";
+import { RiAlertFill } from "react-icons/ri";
+
+import { useEdgeStore } from "@/lib/edgestore";
+import { Alert } from "./myTypeBlocks/alert/Alert";
+import { ChartBlock } from "./myTypeBlocks/charts/chartType";
+import MenuCharts from "./dragHandleMenu/menuCharts/menuCharts";
+import { DocLinkBlock } from "./myTypeBlocks/linkDocs/linkdocsType";
+import { Mention } from "./myInlineContent/Mention";
+import { Charts } from "./myInlineContent/Charts";
+import { TbCirclesRelation } from "react-icons/tb";
+
+import "./styles.css";
+
+const schema = BlockNoteSchema.create({
+  inlineContentSpecs: {
+    // Adds all default inline content.
+    ...defaultInlineContentSpecs,
+    // Adds the mention tag.
+    mention: Mention,
+    chartContent: Charts,
+  },
+  blockSpecs: {
+    ...defaultBlockSpecs,
+    alert: Alert,
+    chart: ChartBlock,
+    docLink: DocLinkBlock,
+  },
+});
+
+// Function which gets all users for the mentions menu.
+const getMentionMenuItems = (
+  editor: typeof schema.BlockNoteEditor
+): DefaultReactSuggestionItem[] => {
+  const users = ["Steve", "Bob", "Joe", "Mike"];
+
+  return users.map((user) => ({
+    title: user,
+    onItemClick: () => {
+      editor.insertInlineContent([
+        {
+          type: "mention",
+          props: {
+            user,
+          },
+        },
+        " ", // add a space after the mention
+      ]);
+    },
+  }));
+};
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
+  const editor = useCreateBlockNote({
+    schema,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    uploadFile: handleUpload,
+  });
+
+  const insertAlert = (editor: typeof schema.BlockNoteEditor) => ({
+    title: "alert",
+    onItemClick: () => {
+      insertOrUpdateBlock(editor, {
+        type: "alert",
+      });
+    },
+    aliases: [
+      "alert",
+      "notification",
+      "emphasize",
+      "warning",
+      "error",
+      "info",
+      "success",
+    ],
+    group: "Other",
+    icon: <RiAlertFill />,
+  });
+
+  const linkDocsBlock = (editor: typeof schema.BlockNoteEditor) => ({
+    title: "docLink",
+    onItemClick: () => {
+      insertOrUpdateBlock(editor, {
+        type: "docLink",
+        props: {
+          backgroundColor: "green",
+          textColor: "default",
+        },
+        content: [
+          {
+            type: "text",
+            text: "🔗" + `Documento relacionado\n`,
+            styles: {
+              bold: true,
+            },
+          },
+          {
+            type: "link",
+            href: `http://localhost:3000/documents/j57dqwpadqjq7x8nrj4cjzndk56nqhj7`,
+            content: [
+              {
+                type: "text",
+                text: `Untitled`,
+                styles: {
+                  textColor: "blue",
+                },
+              },
+            ],
+          },
+        ],
+      });
+      editor.getTextCursorPosition().block, "after";
+      insertOrUpdateBlock(editor, 
+          {
+            type: "paragraph",
+            props: {
+              textColor: "default",
+              backgroundColor: "default",
+            },
+            content: [
+              {
+                type: "text",
+                text: "",
+                styles: {},
+              },
+            ],
+          });
+        editor.getTextCursorPosition().block, "after"
+    },
+    aliases: ["docLink"],
+    group: "Other",
+    icon: <TbCirclesRelation />,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+        editable={editable}
+        slashMenu={false}
+        sideMenu={false}
+        onChange={() => {
+          // Saves the document JSON to state.
+          const blocks = editor.document;
+          onChange(JSON.stringify(blocks, null, 2));
+        }}
+      >
+        <SuggestionMenuController
+          triggerCharacter={"/"}
+          getItems={async (query) =>
+            filterSuggestionItems(
+              [
+                ...getDefaultReactSlashMenuItems(editor),
+                insertAlert(editor),
+                linkDocsBlock(editor),
+              ],
+              query
+            )
+          }
+        />
+        <SuggestionMenuController
+          triggerCharacter={"@"}
+          getItems={async (query) =>
+            // Gets the mentions menu items
+            filterSuggestionItems(getMentionMenuItems(editor), query)
+          }
+        />
+        <SideMenuController
+          sideMenu={(props) => {
+            return props.block.type === "table" ? (
+              <SideMenu
+                {...props}
+                dragHandleMenu={(props) => (
+                  <DragHandleMenu {...props}>
+                    <RemoveBlockItem {...props}>Delete</RemoveBlockItem>
+                    <MenuCharts editor={editor} />
+                  </DragHandleMenu>
+                )}
+              />
+            ) : (
+              <SideMenu
+                {...props}
+                dragHandleMenu={(props) => (
+                  <DragHandleMenu {...props}>
+                    <RemoveBlockItem {...props}>Delete</RemoveBlockItem>
+                    <BlockColorsItem {...props}>Colors</BlockColorsItem>
+                  </DragHandleMenu>
+                )}
+              />
+            );
+          }}
+        />
+      </BlockNoteView>
+    </div>
+  );
+};
+
+export default Editor;
